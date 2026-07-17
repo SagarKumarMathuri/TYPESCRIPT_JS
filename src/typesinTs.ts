1e8ab7d10886b9ffd689f5edd37c7b8b1dd3136a
sagar kumar mathuri <sagarku2002@gmail.com>
2026-07-17 10:53:53 +0530

    typesTs

---
+++ src/typesinTs.ts
@@ -0,0 +1,10 @@
+let drink = "water";
+
+let cup = Math.random() > 0.5 ? 10 : 5;
+
+let isHot: boolean = true;
+
+let temperature: number = 75;
+
+let message: string = `The drink is ${drink} and the cup size is ${cup} ounces.`;
+console.log(message);
